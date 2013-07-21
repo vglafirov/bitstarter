3977eba09db0814c916709d6df5ea84bbffdf74c
Vladimir Glafirov <vglafirov@gmail.com>
2013-07-21 10:33:11 +0000

    file watcher

--- web.js
+++ web.js
@@ -9,20 +9,17 @@ var fileName = "index.html"
 fs.exists(fileName, function(exists) {
     if (exists) {
 	fs.stat(fileName, function(error, stats) {
-	    fs.open(fileName, "r", function(error, fd) {
-		var buffer = new Buffer(stats.size);
-		fs.readFileSync(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
-		    var data = buffer.toString("utf8", 0, buffer.length);
-		    app.get('/', function(request, response) {
-			response.send(data);
-		    });
-		    console.log(data);
-		    fs.close(fd);
-		});
+	    var buffer = new Buffer(stats.size);
+	    buffer.write(fs.readFileSync(fileName, 'utf8'), stats.size)
+	    var data = buffer.toString("utf8", 0, buffer.length);
+	    app.get('/', function(request, response) {
+		response.send(data);
 	    });
 	});
     }
 });
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
