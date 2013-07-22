022b7007e06d56cf3ed1d95dbd96ab0eabda4a86
Ubuntu <ubuntu@ip-10-35-153-95.eu-west-1.compute.internal>
2013-07-22 08:30:56 +0000

    bootstrap

--- web.js
+++ web.js
@@ -20,7 +20,7 @@ fs.exists(fileName, function(exists) {
 
 
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
